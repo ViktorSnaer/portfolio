39b2f28f2a7684fd9b0c2189a397a9c084aee7aa
Berserkur <viktor.snaer23@gmail.com>
2021-08-06 21:59:09 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,59 @@
+// === hamburger-menu === //
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach((n) =>
+  n.addEventListener("click", function () {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
+
+// === scroll === //
+
+document.querySelectorAll("a[href^='#']").forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+      block: "center",
+    });
+  });
+});
+
+// === FADE IN === //
+
+const sliders = document.querySelectorAll(".slide-in");
+
+const appearOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px -200px 0px",
+};
+
+const appearOnScroll = new IntersectionObserver(function (
+  entries,
+  appearOnScroll
+) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+},
+appearOptions);
+
+sliders.forEach((slider) => {
+  appearOnScroll.observe(slider);
+});
